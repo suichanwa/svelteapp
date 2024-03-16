f85f95ab250d01eca18777b074f502b191553c8f
suichanwa <augustinnegura746@gmail.com>
2024-03-16 14:08:41 +0200

    upd

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    /**
+     * @type {any[]}
+     */
+    let tasks = [];
+
+    function addTask() {
+        tasks = [...tasks, { id: tasks.length, text: `Task ${tasks.length}` }];
+
+        console.log(tasks);
+    }
+
+    /**
+     * @param {any} id
+     */
+    function deleteTask(id) {
+        tasks = tasks.filter(task => task.id !== id);
+    }
+</script>
+
+<main>
+  <h1 class='todo'>Todo</h1>
+  <button on:click={addTask}>Add task</button>
+
+  <ul>
+    {#each tasks as task}
+      <li>
+          {task.text}
+          <input type="checkbox">
+            <label for="checkbox">Done</label>
+          <button on:click={() => deleteTask(task.id)}>Delete</button>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+    .todo {
+        font-family: monospace;
+        font-size: 2em;
+        text-align: center;
+        margin-top: 2%;
+    }
+</style>
